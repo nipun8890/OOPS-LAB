c4b526218042a407371b3f405341b06642cb7b8f
Nipun Agrawal <Nipun Agrawal@10.6.2.198>
2025-02-19 01:00:26 +0530

    commit

---
+++ src/Experiment6/MathConstants.java
@@ -0,0 +1,28 @@
+package Experiment6;
+
+public class MathConstants {
+
+}
+package Q;
+
+class MathConstants {
+    final double PI = 3.14159;
+
+    final void displayPI() {
+        System.out.println("Value of PI: " + PI);
+    }
+}
+
+class Circle extends MathConstants {
+    
+    void calculateArea(double radius) {
+        double area = PI * radius * radius;
+        System.out.println("Area of Circle with radius " + radius + " is: " + area);
+    }
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.displayPI();
+        circle.calculateArea(5.0);
+    }
+}
--- src/Experiment6/logger.java
+++ src/Experiment6/logger.java
@@ -3,3 +3,37 @@ package Experiment6;
 public class logger {
 
 }
+//Final class Logger
+class Logger {
+ public void logMessage(String message) {
+     System.out.println(message);
+ }
+
+
+//Attempting to extend the final class Logger - This will result in a compilation error
+/*
+class ExtendedLogger extends Logger {  // This will cause a compile-time error
+ // ...
+}
+*/
+
+//Main class to demonstrate the Logger class
+ public static void main(String[] args) {
+     Logger logger = new Logger();
+     logger.logMessage("This is a sample log message.");
+
+
+     //Demonstrating anonymous inner class (alternative if you *really* needed different behavior)
+     //While you can't *extend* Logger, you *could* create a new class based on it.
+     //This is more complex than usually necessary, but shows how you could get different behavior
+     //without inheritance.
+     Logger specializedLogger = new Logger(){
+         public void logMessage(String message){
+             System.out.println("Specialized Logging: " + message.toUpperCase());
+         }
+     };
+
+     specializedLogger.logMessage("A special message");
+
+ }
+}
